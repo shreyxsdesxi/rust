ddb6a76cdcf9cb8a1daab3c22df2f51b977b00de
shreyas <shreyasmains123@gmail.com>
2024-03-30 13:31:38 +0530

    Hello World

---
+++ hello_cargo/target/debug/deps/hello_cargo-7f46440d24b61c92.d
@@ -0,0 +1,5 @@
+/home/shreyas/rust/hello_cargo/target/debug/deps/hello_cargo-7f46440d24b61c92: src/main.rs
+
+/home/shreyas/rust/hello_cargo/target/debug/deps/hello_cargo-7f46440d24b61c92.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/shreyas/rust/hello_cargo/target/debug/hello_cargo: /home/shreyas/rust/hello_cargo/src/main.rs
